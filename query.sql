f64b2942475aafc9061245d753db4cf9c8dab1ad
diananz3 <dian.anz1365@gmail.com>
2022-06-15 13:03:48 +0700

    add search bar

--- query.sql
+++ query.sql
@@ -1,6 +1,3 @@
 SELECT * FROM hasil 
-INTO OUTFILE '~/nilai/score.csv'
-FIELDS TERMINATED BY ','
-
-mysqldump -u root -p score hasil > score.xls
-mysqldump -u root -p -t -T/nilai score hasil --fields-terminated-by=,
+INTO OUTFILE 'score.csv'
+FIELDS TERMINATED BY ',';
